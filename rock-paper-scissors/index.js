a57c5f838edd82ca3471cc0e2cb3f6787fa984fa
unknown <anhelinapavliuk@gmail.com>
2024-01-16 16:23:59 +0100

    added

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -4,10 +4,56 @@ const resultDisplay = document.getElementById("result");
 const possibleChoices = document.querySelectorAll("button");
 
 let userChoice;
+let computerChoice;
+let result;
 
 possibleChoices.forEach(
   possibleChoice => possibleChoice.addEventListener("click", (e) => {
     userChoice = e.target.id;
     userChoiceDispaly.innerHTML = userChoice;
+    generateComputerChoice();
+    getResult();
   }
 ));
+
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+
+  if (randomNumber === 1) {
+    computerChoice = "rock"
+  }
+  if (randomNumber === 2) {
+    computerChoice = "scissors";
+  }
+  if (randomNumber === 3) {
+    computerChoice = "paper"
+  }
+
+  computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+function getResult() {
+  if (computerChoice === userChoice) {
+    result = "it's a draw!"
+  }
+  if (computerChoice === "rock" && userChoice === "paper") {
+    result = "you win!"
+  }
+  if (computerChoice === "rock" && userChoice === "scissors") {
+    result = "you lose!"
+  }
+  if (computerChoice === "paper" && userChoice === "scissors") {
+    result = "you win!"
+  }
+  if (computerChoice === "paper" && userChoice === "rock") {
+    result = "you lose!"
+  }
+  if (computerChoice === "scissors" && userChoice === "rock") {
+    result = "you win!"
+  }
+  if (computerChoice === "scissors" && userChoice === "paper") {
+    result = "you lose!"
+  }
+
+  resultDisplay.innerHTML = result;
+}
