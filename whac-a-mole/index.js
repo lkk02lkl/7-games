4bc9e31a5a018f6dc381e23656bb0e464acec172
unknown <anhelinapavliuk@gmail.com>
2024-01-17 21:25:38 +0100

    added

---
+++ whac-a-mole/index.js
@@ -0,0 +1,32 @@
+const squares = document.querySelectorAll('.square');
+const mole = document.querySelectorAll('.mole');
+const timeLeft = document.querySelectorAll('#time-left');
+const score = document.querySelectorAll('#scoret');
+
+let result = 0;
+
+function randomSquare() {
+  squares.forEach(square => {
+    square.classList.remove('mole');
+  });
+
+  let randomSquare = squares[Math.floor(Math.random() * 9)];
+  randomSquare.classList.add('mole');
+
+  hitPosition = randomSquare.id;
+}
+
+squares.forEach(square => {
+  square.addEventListener('mousedown', () => {
+    if (square.id === hitPosition) {
+      result++
+    }
+  })
+});
+
+function moveMole() {
+  let timeId = null;
+  timeId = setInterval(randomSquare, 500);
+}
+
+moveMole();
