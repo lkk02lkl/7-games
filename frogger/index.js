ad3008a16de3eee5846386c933c3e7066c7c0610
unknown <anhelinapavliuk@gmail.com>
2024-01-20 13:20:34 +0100

    added

--- frogger/index.js
+++ frogger/index.js
@@ -32,9 +32,11 @@ function moveFrog(e) {
 
 document.addEventListener('keyup', moveFrog);
 
-function autoMOveLogs() {
+function autoMoveElements() {
   logsLeft.forEach(logLeft => moveLogLeft(logLeft));
   logsRight.forEach(logRight => moveLogRight(logRight));
+  carsLeft.forEach((carLeft) => moveCarLeft(carLeft));
+  carsRight.forEach((carRight) => moveCarRight(carRight));
 }
 
 function moveLogLeft(logLeft) {
@@ -66,25 +68,59 @@ function moveLogRight(logRight) {
   switch (true) {
     case logRight.classList.contains("l1"):
       logRight.classList.remove("l1");
-      logRight.classList.add("l2");
+      logRight.classList.add("l5");
       break;
     case logRight.classList.contains("l2"):
       logRight.classList.remove("l2");
-      logRight.classList.add("l3");
+      logRight.classList.add("l1");
       break;
     case logRight.classList.contains("l3"):
       logRight.classList.remove("l3");
-      logRight.classList.add("l4");
+      logRight.classList.add("l2");
       break;
     case logRight.classList.contains("l4"):
       logRight.classList.remove("l4");
-      logRight.classList.add("l5");
+      logRight.classList.add("l3");
       break;
     case logRight.classList.contains("l5"):
       logRight.classList.remove("l5");
-      logRight.classList.add("l1");
+      logRight.classList.add("l4");
       break;
   }
 }
 
-setInterval(autoMOveLogs, 1000);
+function moveCarLeft(carLeft) {
+  switch (true) {
+    case carLeft.classList.contains("c1"):
+      carLeft.classList.remove("c1");
+      carLeft.classList.add("c2");
+      break;
+    case carLeft.classList.contains("c2"):
+      carLeft.classList.remove("c2");
+      carLeft.classList.add("c3");
+      break;
+    case carLeft.classList.contains("c3"):
+      carLeft.classList.remove("c3");
+      carLeft.classList.add("c1");
+      break;
+  }
+}
+
+function moveCarRight(carRight) {
+  switch (true) {
+    case carRight.classList.contains("c1"):
+      carRight.classList.remove("c1");
+      carRight.classList.add("c3");
+      break;
+    case carRight.classList.contains("c2"):
+      carRight.classList.remove("c2");
+      carRight.classList.add("c1");
+      break;
+    case carRight.classList.contains("c3"):
+      carRight.classList.remove("c3");
+      carRight.classList.add("c2");
+      break;
+  }
+}
+
+setInterval(autoMoveElements, 1000);
