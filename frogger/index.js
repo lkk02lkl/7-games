9e710f79bde8898300b8716c208ee69b40c2450a
unknown <anhelinapavliuk@gmail.com>
2024-01-20 12:51:48 +0100

    added

--- frogger/index.js
+++ frogger/index.js
@@ -34,6 +34,7 @@ document.addEventListener('keyup', moveFrog);
 
 function autoMOveLogs() {
   logsLeft.forEach(logLeft => moveLogLeft(logLeft));
+  logsRight.forEach(logRight => moveLogRight(logRight));
 }
 
 function moveLogLeft(logLeft) {
@@ -60,3 +61,30 @@ function moveLogLeft(logLeft) {
       break;
   }
 }
+
+function moveLogRight(logRight) {
+  switch (true) {
+    case logRight.classList.contains("l1"):
+      logRight.classList.remove("l1");
+      logRight.classList.add("l2");
+      break;
+    case logRight.classList.contains("l2"):
+      logRight.classList.remove("l2");
+      logRight.classList.add("l3");
+      break;
+    case logRight.classList.contains("l3"):
+      logRight.classList.remove("l3");
+      logRight.classList.add("l4");
+      break;
+    case logRight.classList.contains("l4"):
+      logRight.classList.remove("l4");
+      logRight.classList.add("l5");
+      break;
+    case logRight.classList.contains("l5"):
+      logRight.classList.remove("l5");
+      logRight.classList.add("l1");
+      break;
+  }
+}
+
+setInterval(autoMOveLogs, 1000);
