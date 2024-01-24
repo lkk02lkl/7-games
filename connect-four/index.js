a859f8b2fe2783ab52e0cecb0fa40d382694020a
unknown <anhelinapavliuk@gmail.com>
2024-01-24 12:03:16 +0100

    added

---
+++ connect-four/index.js
@@ -0,0 +1,4 @@
+const squares = document.querySelectorAll(".grid div");
+const result = document.querySelector("#result");
+const displayCurrentPlayer = document.querySelector("#current-player");
+let currentPlayer = 1;
